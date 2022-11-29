5e89711efc1ab70d7e3f92854d30fad1d14d6c76
Ludek Fedurca <fedurca@gmail.com>
2022-11-29 20:13:20 +0100

    initial commit

--- main.py
+++ main.py
@@ -2,15 +2,22 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+import json
 import requests
+import numpy
+import cv2
+import matplotlib
+import PIL
+import scrapy
+import bs4
+
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
+fcpy = print_hi
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
